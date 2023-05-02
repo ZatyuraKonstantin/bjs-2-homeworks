86915436249ad10e253da7815b016cf6368ca78e
Konstantin Zatyura <19kostik94@mail.ru>
2023-05-03 00:14:30 +0700

    Исправлены ошибки Основные понятия

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,10 +2,8 @@
 
 function solveEquation(a, b, c) {
   const d = b**2-4*a*c;
-  let arr;
-  if (d < 0 ) {
-     arr = [];
-  } else if (d === 0) {
+  let arr = [];
+  if (d === 0) {
     arr = [-b / ( 2 * a )];
   } else if (d > 0) {
     arr = [(-b + Math.sqrt(d) ) / (2*a), (-b - Math.sqrt(d) ) / (2*a)]
@@ -13,14 +11,10 @@ function solveEquation(a, b, c) {
   return arr;
 }
 
-
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  if (!isNaN(percent) && !isNaN(contribution) && !isNaN(amount)) {
   let percentMonth = percent / 100 / 12;
   let bodyCredit = amount - contribution;
   let monthly = bodyCredit * (percentMonth + (percentMonth / (((1 + percentMonth) ** countMonths) - 1)));
   let totalSum = parseFloat((monthly * countMonths).toFixed(2));
   return totalSum;
-  }
-  return false;
 }
